2d9f0f8fe6feff29f85a06c4a31a3953c3a4630c
fakhryhizballah <falehry88@gmail.com>
2022-10-22 13:21:30 +0700

    add ENV

---
+++ env.h
@@ -0,0 +1,20 @@
+// replace MySSID with your WiFi network name
+// ubah file ini menjadi file .h dengan nama secrets.h
+#define SECRET_SSID1 "Spairum.NET"
+#define SECRET_PASS1 "123spairum.net"
+#define SECRET_SSID2 "DAILYMIX"
+#define SECRET_PASS2 "D@ilymix"
+
+// MQTT Broker server ip
+#define MQTT_SERVER "app.spairum.my.id"
+// MQTT Broker server port
+#define MQTT_PORT 1883
+// MQTT Broker username
+#define MQTT_USER "sandbox"
+// MQTT Broker password
+#define MQTT_PASS "testing"
+// MQTT Broker client id
+#define MQTT_CLIENT_ID "Taman"
+
+// id of the device
+#define DEVICE_ID "Taman"
